2c91b342b07b9db44c4f045ccc24ffc4e531e172
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-01-27 20:04:45 -0800

    Class 2

--- test.py
+++ test.py
@@ -55,3 +55,33 @@ me = {
 print(me["last_name"])
 me["last_name"] = "Doe"
 print(me)
+
+ages = [32, 74, 20, 69, 52, 26, 31, 77, 43, 73, 51, 57, 19, 79, 40, 34, 27, 23, 21, 44, 53, 55, 24, 36, 41, 47, 78, 46, 68, 75, 49, 83, 61, 60, 29, 56, 67, 17, 70, 81, 87, 38]
+
+# Print the sum of all numbers
+def example1():
+    sum = 0
+    for age in ages:
+        sum += age
+    print(sum)
+
+# Count how many users are equal or older than 21
+def example2():
+    count = 0
+    for age in ages:
+        if(age >= 21):
+            count += 1
+    print(count)
+
+# How many users are between 30 and 40 years old
+def example3():
+    counter = 0
+    for age in ages:
+        if age >= 30 and age <= 40:
+            counter += 1
+    print(counter)
+
+# Call the functions
+example1()
+example2()
+example3()
